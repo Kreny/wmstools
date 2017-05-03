8f5afa7c99d4618c717a75eec6ad253eb513253d
klepejir <klepejir@WL2010045.eu.tieto.com>
2017-05-03 14:57:31 +0200

    ZIP limit increased

--- model/test_execution.rb
+++ model/test_execution.rb
@@ -16,7 +16,7 @@ class TestExecution
   property :pid, String
   property :exit_code, Integer
   property :stderr, Text
-  property :output_zip_base64, Text, :length => 2000000
+  property :output_zip_base64, Text, :length => 4000000
 
   belongs_to :environment
   belongs_to :test_suite
