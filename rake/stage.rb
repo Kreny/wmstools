74f126698f54306159a42333e95a728829cdec6f
jirkan <30649291+jirkan@users.noreply.github.com>
2018-01-03 13:16:13 +0100

    Update bundle install

--- rake/stage.rb
+++ rake/stage.rb
@@ -46,7 +46,7 @@ class Stage
   end
 
   def bundle_install
-    ssh_exec("rvm use 2.2.2")
+    ssh_exec("source /etc/profile.d/rvm.sh; rvm use 2.2.2")
     ssh_exec("cd #{@pa.th}; bundle install")
   end
 
