d3f9e776ebabd0171929b32dc4030d007df778ab
jirkan <30649291+jirkan@users.noreply.github.com>
2018-01-05 14:41:58 +0100

    Change hostname

--- agent/config/conf_production.rb
+++ agent/config/conf_production.rb
@@ -1,5 +1,5 @@
 DATA_SOURCE = 'mysql://wmsuser:SalvatorDali01@uw001837/wmstools'
-THIS_AGENT_ID = 'uw001685'
+THIS_AGENT_ID = 'NMA02'
 SOAPUI_HOME = '/home/wmstools/work/wmsTools/soapui' # without trailing slash
 SVN_HOME = '/home/wmstools/work/wmsTools/svn' # without trailing slash
 JAVA_HOME = "#{SOAPUI_HOME}/jre/bin/java" # without trailing slash
