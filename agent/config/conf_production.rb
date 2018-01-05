e286b6d3a383d1a5d1dd93def2a50caac1d3abc3
jirkan <30649291+jirkan@users.noreply.github.com>
2018-01-05 14:35:21 +0100

    Update conf_production.rb

--- agent/config/conf_production.rb
+++ agent/config/conf_production.rb
@@ -1,6 +1,6 @@
 DATA_SOURCE = 'mysql://wmsuser:SalvatorDali01@uw001837/wmstools'
 THIS_AGENT_ID = 'uw001685'
-SOAPUI_HOME = '/root/work/wmsTools/soapui' # without trailing slash
-SVN_HOME = '/root/work/wmsTools/svn' # without trailing slash
+SOAPUI_HOME = '/home/wmstools/work/wmsTools/soapui' # without trailing slash
+SVN_HOME = '/home/wmstools/work/wmsTools/svn' # without trailing slash
 JAVA_HOME = "#{SOAPUI_HOME}/jre/bin/java" # without trailing slash
 QUOTA = 3
