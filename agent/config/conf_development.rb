1ddb25bd590f5614c2e47a32743c20276f353137
jirkan <30649291+jirkan@users.noreply.github.com>
2018-01-16 12:37:31 +0100

    Revert 1

--- agent/config/conf_development.rb
+++ agent/config/conf_development.rb
@@ -1,9 +1,9 @@
 DATA_SOURCE = 'mysql://wmsuser:SalvatorDali01@localhost/wmstools_staging'
 THIS_AGENT_ID = 'localhost'
-SOAPUI_HOME = '/home/wmstools/work/wmsTools/soapui' # without trailing slash
-SVN_HOME = '/home/wmstools/work/wmsTools/svn' # without trailing slash
+SOAPUI_HOME = '/root/work/wmsTools/soapui' # without trailing slash
+SVN_HOME = '/root/work/wmsTools/svn' # without trailing slash
 JAVA_HOME = "#{SOAPUI_HOME}/jre/bin/java" # without trailing slash
 JAR_HOME = '/TestAutomation'
-FAKE_OUTPUT_HOME = '/home/wmstools/work/temp'
+FAKE_OUTPUT_HOME = '/root/work/temp'
 QUOTA = 3
 
