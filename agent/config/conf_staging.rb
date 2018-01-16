434926694cdab341397125ba79b174fca3b54e2d
jirkan <30649291+jirkan@users.noreply.github.com>
2018-01-16 12:42:13 +0100

    Revert 1

--- agent/config/conf_staging.rb
+++ agent/config/conf_staging.rb
@@ -1,7 +1,7 @@
-DATA_SOURCE = 'mysql://wmsuser:SalvatorDali01@NMA02/wmstools_staging'
-THIS_AGENT_ID = 'NMA02_staging'
-SOAPUI_HOME = '/home/wmstools/work/wmsTools/soapui' # without trailing slash
-SVN_HOME = '/home/wmstools/work/wmsTools/svn' # without trailing slash
+DATA_SOURCE = 'mysql://wmsuser:SalvatorDali01@uw001685/wmstools_staging'
+THIS_AGENT_ID = 'uw001685_staging'
+SOAPUI_HOME = '/root/work/wmsTools/soapui' # without trailing slash
+SVN_HOME = '/root/work/wmsTools/svn' # without trailing slash
 JAVA_HOME = "#{SOAPUI_HOME}/jre/bin/java" # without trailing slash
-JAR_HOME = '/home/wmstools/work/wmsTools/svn/trunk/TestAutomationDemo'
+JAR_HOME = '/root/work/wmsTools/svn/trunk/TestAutomationDemo'
 QUOTA = 5
