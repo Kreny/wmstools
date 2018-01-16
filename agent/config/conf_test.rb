23366dc9ab80ef99cb5f44d80ddfa4cceca671dc
jirkan <30649291+jirkan@users.noreply.github.com>
2018-01-16 12:43:31 +0100

    Revert 1

--- agent/config/conf_test.rb
+++ agent/config/conf_test.rb
@@ -1,4 +1,4 @@
-THIS_AGENT_ID = 'NMA02'
+THIS_AGENT_ID = 'uw001685'
 SOAPUI_HOME = '/home/nodeTest/work/soapui' # without trailing slash
 SVN_HOME = '/home/nodeTest/work/svn' # without trailing slash
 JAVA_HOME = '/usr/bin/java' # without trailing slash
