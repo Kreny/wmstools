08e4c559ae96be874576c81cbf10821ff67d9c6f
jirkan <30649291+jirkan@users.noreply.github.com>
2018-02-04 16:58:49 +0100

    Change DNS to IP.

--- agent/config/conf_staging.rb
+++ agent/config/conf_staging.rb
@@ -1,4 +1,4 @@
-DATA_SOURCE = 'mysql://wmsuser:SalvatorDali01@NMA02/wmstools_staging'
+DATA_SOURCE = 'mysql://wmsuser:SalvatorDali01@10.0.219.120/wmstools_staging'
 THIS_AGENT_ID = 'NMA02_staging'
 SOAPUI_HOME = '/home/wmstools/work/wmsTools/soapui' # without trailing slash
 SVN_HOME = '/home/wmstools/work/wmsTools/svn' # without trailing slash
