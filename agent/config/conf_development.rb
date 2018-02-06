ba8f3640265c159c83dca20f0c232cced06c65da
jirkan <30649291+jirkan@users.noreply.github.com>
2018-02-06 15:02:04 +0100

    Update SVN path

--- agent/config/conf_development.rb
+++ agent/config/conf_development.rb
@@ -1,7 +1,7 @@
 DATA_SOURCE = 'mysql://wmsuser:SalvatorDali01@localhost/wmstools_staging'
 THIS_AGENT_ID = 'localhost'
 SOAPUI_HOME = '/home/wmstools/work/soapui' # without trailing slash
-SVN_HOME = '/home/wmstools/work/wmsTools/svn' # without trailing slash
+SVN_HOME = '/home/wmstools/work/svn' # without trailing slash
 JAVA_HOME = "#{SOAPUI_HOME}/jre/bin/java" # without trailing slash
 JAR_HOME = '/TestAutomation'
 FAKE_OUTPUT_HOME = '/home/wmstools/work/temp'
